4ceea09b805678ad9fef105b773fe1bbb85b1a86
Manas <99756067+scienmanas@users.noreply.github.com>
2023-10-10 07:19:55 +0530

    Initial display changed

--- Buttons/lib/main.dart
+++ Buttons/lib/main.dart
@@ -1,139 +1,145 @@
 import 'dart:io';
-
+import 'package:lottie/lottie.dart';
 import 'package:buttons/screens/level2.dart';
-import 'package:buttons/screens/pageofinstructions.dart';
 import 'package:flutter/material.dart';
+import 'package:buttons/screens/pageofinstructions.dart';
 import 'package:buttons/screens/level1.dart';
 import 'package:buttons/screens/level3.dart';
 import 'package:buttons/screens/level4.dart';
 import 'package:buttons/screens/level5.dart';
 import 'package:buttons/screens/FinalLevel.dart';
 
-// import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'Navigation Basics',
+    title: 'Pairing Game',
     home: FirstRoute(),
   ));
 }
 
 class FirstRoute extends StatelessWidget {
-  const FirstRoute({super.key});
+  const FirstRoute({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('First Route'),
-      // ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/background_image0.png"),
-            fit: BoxFit.fill,
+      appBar: AppBar(
+        title: Text(
+          'Pairing Game',
+          style: TextStyle(
+            fontSize: 24, // Adjust the font size
+            fontWeight: FontWeight.bold, // Bold text
+            color: Colors.white, // Text color
+            letterSpacing: 1.5, // Adjust letter spacing
           ),
         ),
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
+        centerTitle: true, // Center the app name
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.purple], // Adjust gradient colors
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  child: Text('Start'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                       MaterialPageRoute(
-                        builder: (context) => HelpSplashScreen(text: "Find all word", imagePath: "assets/3danimations/correct_ans.json", levelName: 1),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    child: Image.asset(
+                        "assets/IIT.png"), // Replace with your image asset
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      "X",
+                      style: TextStyle(
+                        fontSize: 48.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // Text color
                       ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(250, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )),
-                  // onPressed: () {},
-                  // child: const Text("Exit"),
+                    ),
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    child: Image.asset(
+                        "assets/II.png"), // Replace with your image asset
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Lottie.asset(
+                'assets/3danimations/start_right.json',
+                width: 300,
+                height: 300,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                child: Text(
+                  'Start',
+                  style: TextStyle(fontSize: 18), // Button text style
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HelpSplashScreen(
+                        text: "Find all word",
+                        imagePath: "assets/3danimations/correct_ans.json",
+                        levelName: 1,
+                      ),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(250, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  backgroundColor: Colors.green, // Button background color
                 ),
               ),
-              // Padding(
-              //   padding: const EdgeInsets.all(16.0),
-              //   child: ElevatedButton(
-              //     child: Text('Temp1'),
-              //     onPressed: () {
-              //       Navigator.push(
-              //         context,
-              //         MaterialPageRoute(
-              //           builder: (context) => CrosswordApp(),
-              //         ),
-              //       );
-              //     },
-              //     style: ElevatedButton.styleFrom(
-              //         fixedSize: const Size(250, 60),
-              //         shape: RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.circular(30),
-              //         )),
-              //   ),
-              // ),
-              // Padding(
-              //   padding: const EdgeInsets.all(16.0),
-              //   child: ElevatedButton(
-              //     child: Text('Temp2'),
-              //     onPressed: () {
-              //       Navigator.push(
-              //         context,
-              //         MaterialPageRoute(
-              //           builder: (context) => const FourthRoute(),
-              //         ),
-              //       );
-              //     },
-              //     style: ElevatedButton.styleFrom(
-              //         fixedSize: const Size(250, 60),
-              //         shape: RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.circular(30),
-              //         )),
-              //   ),
-              // ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  child: Text('Exit'),
-                  onPressed: () {
-                    exit(0);
-                  },
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(250, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                child: Text(
+                  'Exit',
+                  style: TextStyle(fontSize: 18), // Button text style
                 ),
+                onPressed: () {
+                  exit(0);
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(250, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  backgroundColor: Colors.red, // Button background color
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
               ),
             ],
           ),
         ),
-        // child: Center(
-        //   child: ElevatedButton(
-        //     child: Text('Start'),
-        //     onPressed: () {
-        //       Navigator.push(
-        //         context,
-        //         MaterialPageRoute(builder: (context) => const SecondRoute()),
-        //       );
-        //     },
-        //     style: ElevatedButton.styleFrom(
-        //         fixedSize: const Size(250, 60),
-        //         shape: RoundedRectangleBorder(
-        //           borderRadius: BorderRadius.circular(30),
-        //         )),
-        //   ),
       ),
     );
   }
@@ -169,16 +175,7 @@ class ThirdRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Level 2'),
       ),
-      body:
-          // Center(
-          //   child: ElevatedButton(
-          //     onPressed: () {
-          //       Navigator.pop(context);
-          //     },
-          //     child: const Text('Go back fool!'),
-          // ),
-          Container(
-        // color: Colors.grey,
+      body: Container(
         height: 400,
         child: ListView(
           padding: const EdgeInsets.all(16.0),
@@ -246,11 +243,6 @@ class FourthRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: const Text('Level 2'),
-    //   ),
-    // );
     return MaterialApp(
       home: LevelTwo(),
     );
